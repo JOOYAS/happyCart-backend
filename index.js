99054704576d281873d373068928a82e43236759
jooyas <sayoojsayu428@gmail.com>
2024-10-02 23:09:05 +0530

    cors access localhost for testing

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const port = 3000;
 
 const app = express();
 //middlewares
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
